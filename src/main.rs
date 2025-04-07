1d46078fae77ed517118b92dfcf0483dc8f6b0c8
rahul <sai7rahul7@gmail.com>
2025-04-07 13:20:10 +0530

    Pattern Matching

--- src/main.rs
+++ src/main.rs
@@ -87,12 +87,17 @@ pub fn main() {
         width :30,
         height :40,
     };
-    print!("The area of the rectangle is : {}", rect.area() );
+    println!("The area of the rectangle is : {}", rect.area() );
 
     //pattern matching in rust
     let circle = Shape::Circle(5.0);
     let square = Shape::Square(5.0);
     let rectangle = Shape::Rectangle(3.0, 6.0);
+
+
+    println!("Area of Circle: {}", calculate_area(circle));
+    println!("Area of Square: {}", calculate_area(square));
+    println!("Area of Rectangle: {}", calculate_area(rectangle));
 
 
 }
@@ -146,8 +151,16 @@ impl Rect{
 }
 
 //enum
-enum Shape{
+enum Shape {
     Circle(f64),
     Square(f64),
     Rectangle(f64, f64),
+}
+
+fn calculate_area(shape: Shape) -> f64{
+    match shape{
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Square(length) => length*length,
+        Shape::Rectangle(width, height) => width* height,
+    }
 }
